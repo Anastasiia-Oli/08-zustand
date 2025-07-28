12b2fa8f80194aa0d39d1903aabbe32dd939c1a5
Anastasiia Oliinyk <aolejnik916@gmail.com>
2025-07-28 22:37:00 +0200

    Initial commit

---
+++ app/notes/[id]/page.tsx
@@ -0,0 +1,22 @@
+import { QueryClient, dehydrate } from "@tanstack/react-query";
+import { fetchNoteById } from "@/lib/api";
+import NoteDetails from "./NoteDetails.client";
+
+type NoteDetailsPageProps = {
+  params: Promise<{ id: string }>;
+};
+
+const NoteDetailsPage = async ({ params }: NoteDetailsPageProps) => {
+  const queryClient = new QueryClient();
+  const { id } = await params;
+  const noteId = String(id);
+
+  await queryClient.prefetchQuery({
+    queryKey: ["note", noteId],
+    queryFn: () => fetchNoteById(noteId),
+  });
+
+  return <NoteDetails dehydratedState={dehydrate(queryClient)} />;
+};
+
+export default NoteDetailsPage;
